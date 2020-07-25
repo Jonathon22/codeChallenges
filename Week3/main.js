3e3f84e0ed039a13b726928394c7a538f28d5b9f
Jonathon Joyner <jonathonandon@gmail.com>
2020-07-25 13:56:08 -0500

    changed code for testing

--- Week3/main.js
+++ Week3/main.js
@@ -26,18 +26,27 @@ const newModulo = 21 % 5 === 1;
 //Do not change any of the function names
 
 // 7
+// let str = "string"
+
 function returnString(str) {
  return str
   //simply return the string provided: str
 }
 
+
+
 // 8
+// let x = 3;
+// let y = 6;
+
 function add(x, y) {
-  return x + y
+  return x + y;
   // x and y are numbers
   // add x and y together and return the value
   // code here
+  // console.log(add);
 }
+
 
 // 9
 function subtract(x, y) {
@@ -64,6 +73,8 @@ function divide(x, y) {
 function areEqual(x, y) {
   if (x === y){
   return true 
+  }else {
+    return false
   }
   // return true if x and y are the same
   // otherwise return false
@@ -74,16 +85,22 @@ function areEqual(x, y) {
 function areSameLength(str1, str2) {
   if(str1.length === str2.length){
     return true
+  }else {
+    return false
   }
   // return true if the two strings have the same length
   // otherwise return false
   // code here
 }
 
+
+
 // 14
 function lessThanNinety(num) {
   if(num < 90 ) {
     return true
+  } else {
+    return false
   }
   // return true if the function argument: num , is less than ninety
   // otherwise return false
@@ -94,6 +111,8 @@ function lessThanNinety(num) {
 function greaterThanFifty(num) {
   if(num > 50){
     return true
+  } else {
+    return false
   }
   // return true if num is greater than fifty
   // otherwise return false
@@ -111,7 +130,9 @@ function getRemainder(x, y) {
 function isEven(num) {
   if(num%2 === 0) {
     return true 
-  }
+  } else {
+    return false
+  } 
   // return true if num is even
   // otherwise return false
   // code here
@@ -121,11 +142,14 @@ function isEven(num) {
 function isOdd(num) {
   if (num%2 !== 0){
     return true 
-  }
+  } else {
+    return false 
+  } 
   // return true if num is odd
   // otherwise return false
   // code here
 }
+
 
 // 19
 function square(num) {
@@ -133,39 +157,39 @@ function square(num) {
   // square num and return the new value
   // hint: NOT square root!
   // code here
-}
+} 
 
 // 20
 function cube(num) {
   return num * num * num
   // cube num and return the new value
   // code here
-}
+} 
 
 // 21
 function raiseToPower(num, exponent) {
-  return math.pow(num, exponent);
+  return Math.pow(num, exponent);
   // raise num to whatever power is passed in as exponent
   // code here
-}
+} 
 
 // 22
 function roundNumber(num) {
-return math.round(num)
+return Math.round(num);
   // round num and return it
   // code here
-}
+} 
 
 // 23
 function roundUp(num) {
-  return math.ceil(num)
+  return Math.ceil(num)
   // round num up and return it
   // code here
-}
+} 
 
 // 24
 function addExclamationPoint(str) {
-  return str + "!"
+  return str + '!'
   // add an exclamation point to the end of str and return the new string
   // 'hello world' -> 'hello world!'
   // code here
@@ -173,7 +197,7 @@ function addExclamationPoint(str) {
 
 // 25
 function combineNames(firstName, lastName) {
-  return firstName + " " + lastName
+  return firstName + ' ' + lastName
   // return firstName and lastName combined as one string and separated by a space.
   // 'NSS', 'Bootcamp' -> 'NSS Bootcamp'
   // code here
@@ -181,7 +205,7 @@ function combineNames(firstName, lastName) {
 
 // 26
 function getGreeting(name) {
-  return "hello " + name
+  return 'Hello ' + name
   // Take the name string and concatenate other strings onto it so it takes the following form:
   // 'Sam' -> 'Hello Sam!'
   // code here
@@ -202,4 +226,4 @@ function getTriangleArea(base, height) {
   return base * height / 2
   // return the area of the triangle by using base and height
   // code here
-}
+} 
